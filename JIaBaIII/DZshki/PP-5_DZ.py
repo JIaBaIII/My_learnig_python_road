4ac1091e91a1808d6d24052d6d5a854ea1135b2a
Legion <de-lyubimov@mail.ru>
2021-07-05 23:06:11 +0300

    solve mostly of PP-5 homework, except 2 and 4((

--- JIaBaIII/DZshki/PP-5_DZ.py
+++ JIaBaIII/DZshki/PP-5_DZ.py
@@ -33,19 +33,20 @@
 # 2 Дан класс Pupil
 # Добавьте метод, который позволит ученику забыть какую то часть знаний (не все сразу).
 
-class Pupil:
-    def __init__(self):
-        self.knowledge = []
-
-    def take(self, info):
-        self.knowledge.append(info)
-
-    def forget_some(self, i_want_to_forget):
-        self.knowledge.remove(i_want_to_forget)
-
-
-pup = Pupil
-somelist = [123, 11, 44]
+# class Pupil:
+#     def __init__(self):
+#         self.knowledge = []
+#         print(pup)
+#
+#     def take(self, info):
+#         self.knowledge.append(info)
+#
+#     def forget_some(self, i_want_to_forget):
+#         self.knowledge.remove(i_want_to_forget)
+#
+#
+# pup = Pupil
+# somelist = [123, 11, 44]
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -57,14 +58,65 @@ somelist = [123, 11, 44]
 # гомосексуализма котятам и возвращаться None.
 
 # class Cat:
-#     def __init__(self):
+#     def __init__(self, name, sex, weight):
+#         self.name = name
+#         self.sex = sex
+#         self.weight = weight
 #
-
+#     def __str__(self):
+#         if self.sex == 'male' or self.sex == 'M' or self.sex == 'm':
+#             return 'Кошак (Имя: {}, Пол: {}, Вес: {} гр.)'.format(self.name, self.sex, self.weight)
+#         else:
+#             return 'Кошка (Имя: {}, Пол: {}, Вес: {} гр.)'.format(self.name, self.sex, self.weight)
+#
+#     def __add__(self, gen2):
+#         if self.sex == gen2.sex:
+#             print('Мы тут таких не любим... Котятки будут только у гетеро пары, попробуй еще')
+#             return None
+#         else:
+#             if self.sex == 'male' or self.sex == 'M' or self.sex == 'm':
+#                 child_name_start = gen2.name[:3]
+#                 child_name_end = self.name[-3:]
+#                 return Cat(child_name_start + child_name_end, self.sex, int((self.weight + gen2.weight) / 2))
+#             else:
+#                 child_name_start = gen2.name[:3]
+#                 child_name_end = self.name[-4:]
+#                 return Cat(child_name_start + child_name_end, self.sex, int((self.weight + gen2.weight) / 2))
+#
+#
+# cat_1 = Cat('Эпифинья', 'female', 1000)
+# cat_2 = Cat('Сучок', 'male', 10000)
+#
+# print(cat_1)
+# print(cat_2)
+# print(f'Котенок: {cat_1 + cat_2}')
 
 # --------------------------------------------------------------------------------------------------------------------
 # 4 Написать программу, которая будет сравнивать произвольное количество кошек и сортировать их в порядке возрастания
 # по параметру вес. Эффективность - поебать, но чем больше тем лучше.
 
+# import random
+#
+#
+# class RandomCatAmount:
+#     def __init__(self, arg1, weight=None):
+#         self.weight = weight
+#         self.arg1 = arg1
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __int__(self):
+#         list_of_cats = []
+#         if self.weight is None:
+#             for new_cat in range(0, self.arg1):
+#                 weight = random.randint(2000, 6000)
+#                 list_of_cats.append(weight)
+#         return list_of_cats
+#
+#
+# rca = RandomCatAmount(300)
+# print(sorted(rca.__int__()))                                              # Вангую - Убанское неправильное решение....
 
 # --------------------------------------------------------------------------------------------------------------------
 # 5  Hard Mode. Задачу нагло спиздил, если честно, но она клевая.
@@ -73,3 +125,44 @@ somelist = [123, 11, 44]
 # не теряет. У того, кого бьют, оно уменьшается на 20 очков от одного удара. После каждого удара надо выводить
 # сообщение, какой юнит атаковал, и сколько у противника осталось здоровья. Как только у кого-то заканчивается ресурс
 # здоровья, программа завершается сообщением о том, кто одержал победу.
+
+import random
+
+
+class Warrior:
+    turn = random.randint(1, 2)
+
+    def __init__(self, name, hp=100):
+        self.name = name
+        self.hp = hp
+
+    def __iter__(self):
+        return self
+
+    def __and__(self, other):
+        while self.hp > 0 and other.hp > 0:
+            turn = random.randint(1, 2)
+            print(turn)
+            if turn == 1:
+                other.hp -= 20
+                print(f'Бесстрашный воин {self.name} бьет беднягу \033[35m{other.name}\033[0m на 20 единиц урона!!!!'
+                      f' Отчего у \033[35m{other.name}\033[0m остается \033[35m{other.hp}\033[0m здоровья')
+            elif turn == 2:
+                self.hp -= 20
+                print(f'Ооооох, {other.name} совершает сокрушительную атаку, отнимая у \033[34m{self.name}\033[0m '
+                      f'целых 20 единиц '
+                      f'урона!!! Напряжение наростает, в то время как у \033[34m{self.name}\033[0m осталось'
+                      f' \033[34m{self.hp}\033[0m здоровья')
+        else:
+            if self.hp > other.hp:
+                print(f'У нас победиль !! \033[34m{self.name}\033[0m устоявший на ногах с'
+                      f' \033[34m{self.hp}\033[0m запасом здоровья!')
+            else:
+                print(f'У нас победиль !! \033[35m{other.name}\033[0m устоявший на ногах с'
+                      f' \033[35m{other.hp}\033[0m запасом здоровья!')
+
+
+war_1 = Warrior('Пафнутий')
+war_2 = Warrior('Игорь')
+
+war_1 & war_2
